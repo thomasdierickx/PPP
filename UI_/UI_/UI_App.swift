37b05b8ce24c65c74d7ad1a1148e88a72e1d1bed
Thomas Dierickx <thomas.dierickx3@student.howest.be>
2023-01-19 12:22:55 +0100

    Adding basic user interface

---
+++ UI_/UI_/UI_App.swift
@@ -0,0 +1,17 @@
+//
+//  UI_App.swift
+//  UI_
+//
+//  Created by Thomas Dierickx on 19/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct UI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
