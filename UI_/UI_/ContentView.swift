f6747ae4186cb111edcdceb4063555100650397f
Thomas Dierickx <thomas.dierickx3@student.howest.be>
2023-01-23 11:35:58 +0100

    In Progress #5

--- UI_/UI_/ContentView.swift
+++ UI_/UI_/ContentView.swift
@@ -9,43 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Image("Background")
-            VStack {
-//                Image(systemName: "globe")
-//                    .imageScale(.large)
-//                    .foregroundColor(.accentColor)
-                Text("Make your own")
-                    .font(.system(size: 25) .weight(.bold))
-                    .font(.custom("NunitoSans", size: 25))
-                    .foregroundColor(Color("DarkBlue"))
-                Text("T-SHIRT")
-                    .font(.system(size: 86) .weight(.heavy))
-                    .foregroundColor(Color("DarkBlue"))
-                GeometryReader { geometry in
-                    ImageCarouselView(numberOfImages: 3) {
-                        Image("T-shirt1")
-                            .resizable()
-                            .frame(width: geometry.size.width, height: geometry.size.height)
-                        Image("T-shirt2")
-                            .resizable()
-                            .frame(width: geometry.size.width, height: geometry.size.height)
-                        Image("T-shirt3")
-                            .resizable()
-                            .frame(width: geometry.size.width, height: geometry.size.height)
+        NavigationView {
+            ZStack {
+                Image("Background")
+                VStack {
+                    Text("Make your own")
+                        .font(.system(size: 25) .weight(.bold))
+                        .font(.custom("NunitoSans", size: 25))
+                        .foregroundColor(Color("DarkBlue"))
+                    Text("T-SHIRT")
+                        .font(.system(size: 86) .weight(.heavy))
+                        .foregroundColor(Color("DarkBlue"))
+                    GeometryReader { geometry in
+                        ImageCarouselView(numberOfImages: 3) {
+                            Image("T-shirt1")
+                                .resizable()
+                                .frame(width: geometry.size.width, height: geometry.size.height)
+                            Image("T-shirt2")
+                                .resizable()
+                                .frame(width: geometry.size.width, height: geometry.size.height)
+                            Image("T-shirt3")
+                                .resizable()
+                                .frame(width: geometry.size.width, height: geometry.size.height)
+                        }
+                    }.frame(width: 400, height: 450, alignment: .center)
+                    NavigationLink(destination: StepView()) {
+                        Button(action: { }) {
+                            Text("BEGIN")
+                                .font(.system(size: 20) .weight(.bold))
+                                .foregroundColor(Color("White"))
+                        }
+                        .frame(maxWidth: 300, maxHeight: 50)
+                        .background(Color("LightBlue"))
+                        .cornerRadius(30)
                     }
-                }.frame(width: 400, height: 450, alignment: .center)
-                Button(action: { }) {
-                    Text("BEGIN")
-                        .font(.system(size: 20) .weight(.bold))
-                        .foregroundColor(Color("White"))
+                    .padding()
                 }
-                .frame(maxWidth: 300, maxHeight: 50)
-                .background(Color("LightBlue"))
-                .cornerRadius(30)
+                .padding()
+                .navigationBarTitleDisplayMode(.inline)
             }
-            .padding()
         }
+    }
+}
+
+struct ResultView: View {
+    var body: some View {
+        Text("ResultView")
     }
 }
 
---
+++ UI_/UI_/StepView.swift
@@ -0,0 +1,83 @@
+//
+//  StepView.swift
+//  UI_
+//
+//  Created by Thomas Dierickx on 23/01/2023.
+//
+
+import SwiftUI
+
+struct StepView: View {
+    var body: some View {
+        NavigationView {
+            ZStack {
+                Image("Background")
+                VStack {
+                    Text("Make your own")
+                        .font(.system(size: 25) .weight(.bold))
+                        .font(.custom("NunitoSans", size: 25))
+                        .foregroundColor(Color("DarkBlue"))
+                    Text("T-SHIRT")
+                        .font(.system(size: 86) .weight(.heavy))
+                        .foregroundColor(Color("DarkBlue"))
+                    VStack {
+                        Text("Step1.")
+                            .font(.system(size: 25) .weight(.bold))
+                            .font(.custom("NunitoSans", size: 25))
+                            .foregroundColor(Color("DarkBlue"))
+                            .frame(maxWidth: 350, alignment: .leading)
+                        Text("Choose 3 to 5 different images from your gallery")
+                            .font(.system(size: 25) .weight(.regular))
+                            .font(.custom("NunitoSans", size: 25))
+                            .foregroundColor(Color("DarkBlue"))
+                            .frame(maxWidth: 350, alignment: .leading)
+                    }
+                    .padding()
+                    VStack {
+                        Text("Step2.")
+                            .font(.system(size: 25) .weight(.bold))
+                            .font(.custom("NunitoSans", size: 25))
+                            .foregroundColor(Color("DarkBlue"))
+                            .frame(maxWidth: 350, alignment: .leading)
+                        Text("The program will automatically give you a generated image based on the images you sent in.")
+                            .font(.system(size: 25) .weight(.regular))
+                            .font(.custom("NunitoSans", size: 25))
+                            .foregroundColor(Color("DarkBlue"))
+                            .frame(maxWidth: 350, alignment: .leading)
+                    }
+                    .padding()
+                    VStack {
+                        Text("Step3.")
+                            .font(.system(size: 25) .weight(.bold))
+                            .font(.custom("NunitoSans", size: 25))
+                            .foregroundColor(Color("DarkBlue"))
+                            .frame(maxWidth: 350, alignment: .leading)
+                        Text("Once step 2 is done, you can \n- Reset the process again \n- Change the style \n- Look at the end result")
+                            .font(.system(size: 25) .weight(.regular))
+                            .font(.custom("NunitoSans", size: 25))
+                            .foregroundColor(Color("DarkBlue"))
+                            .frame(maxWidth: 350, alignment: .leading)
+                    }
+                    .padding()
+                    NavigationLink(destination: ResultView()) {
+                        Button(action: { }) {
+                            Text("BEGIN")
+                                .font(.system(size: 20) .weight(.bold))
+                                .foregroundColor(Color("White"))
+                        }
+                        .frame(maxWidth: 300, maxHeight: 50)
+                        .background(Color("LightBlue"))
+                        .cornerRadius(30)
+                    }
+                    .padding()
+                }
+            }
+        }
+    }
+}
+
+struct StepView_Previews: PreviewProvider {
+    static var previews: some View {
+        StepView()
+    }
+}
