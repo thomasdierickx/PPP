4a0d2fc7a1c046892d27b962176b3ee239cb5cc8
Thomas Dierickx <thomas.dierickx3@student.howest.be>
2023-01-25 15:45:09 +0100

    #13 Done, simply deleting 'landscape' mode in settings

---
+++ UI_/UI_/RemoveBG.swift
@@ -0,0 +1,22 @@
+//
+//  RemoveBG.swift
+//  UI_
+//
+//  Created by Thomas Dierickx on 25/01/2023.
+//
+
+import SwiftUI
+
+struct RemoveBG: View {
+//    @Binding var image: [UIImage]
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RemoveBG_Previews: PreviewProvider {
+    static var imageArr: [UIImage]?
+    static var previews: some View {
+        RemoveBG()
+    }
+}
